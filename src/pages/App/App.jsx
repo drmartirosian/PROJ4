82d2ba193fb9d278127254e28aa1096988da96ce
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-17 08:06:49 -0600

    Starter nav

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,19 +4,29 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import Scooter from "../../components/Scooter/Scooter"
 
+
+const Test = [ "Daniel" ]
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {name:"Daniel"};
+    this.state = {
+      test: Test
+    };
   }
   render() {
     return (
       <div>
+        <nav>
+          <th>Home</th>
+          <th>About</th>
+          <th>LOGIN/OUT</th>
+        </nav>
         <header>Header</header>
         <Scooter/>
         <Switch>
-          <Route exact path='/' render={() => 
-            <HomePage />
+          <Route exact path='/' render={() =>
+            <HomePage test={this.state.test} />
           } />
         </Switch>
       </div>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../HomePage/HomePage.css';
+import Scooter from "../../components/Scooter/Scooter"
 
-const Scooter = (props) => {
+const HomePage = ({ test }) => {
   return (
     <div>
         <h3>HOME PAGE</h3>
+        <Scooter />
+  <p>
+  {test}
+  </p>
     </div>
   );
 
 };
 
-export default Scooter;
+export default HomePage;
