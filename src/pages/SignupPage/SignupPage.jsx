a747238e164fb5a4ae7e1c26d15b37a491080501
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-17 16:45:43 -0600

    Signup Form added, auth may not work

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SignupForm from '../../pages/SignupPage/SignupPage';
+import SignupForm from '../../components/SignupForm/SignupForm';
 // import './SignupPage.css';
 
 class SignupPage extends Component {
